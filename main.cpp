2d532b4de18653121edc35b32c7684c87a6495b3
chiar0 <97366638+chiar0@users.noreply.github.com>
2022-03-02 17:50:55 +0100

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string.h>
+// !! C++ 23 !!
+int main()
+{
+    class fortnite_user
+    {
+    protected:
+        int health;
+        int dmg;
+        char icon[3];
+    public:
+        fortnite_user (int health = 5, int dmg = -1)
+        {
+            this->health = health;
+            this->dmg = dmg;
+            icon[0] = 'P';
+            icon[1] = '\0';
+        }
+        void setter (int health, int dmg)   //per assegnare un nuovo valore a un solo paramentro assegnare "0" all'altro
+        {
+            if (health != 0)
+                this->health=health;
+            if (dmg != 0)
+                this->dmg=dmg;
+        }
+        void health_changer(int dmg = -1)   //in base al segno di dmg incrementa o decrementa
+        {
+            this->health += dmg;
+        }
+        void dmg_changer(int dmg = -1)  //in base al segno di dmg incrementa o decrementa
+        {
+            this->health += dmg;
+        }
+    };
+    class fortnite_enemy : public fortnite_user
+    {
+    public:
+        fortnite_enemy (int health = 5, int dmg = -1) : fortnite_user (health, dmg)
+        {
+            this->health = health;
+            this->dmg = dmg;
+            icon[0] = '@';
+            icon[1] = '@';
+            icon[2] = '\0';
+        }
+    };
+}
